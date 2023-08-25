02f78d7cbe9e72393ccb55be9ca1d107136b256a
Lissa Takahashi <99764365+lissatakahashi@users.noreply.github.com>
2023-08-25 20:41:14 -0300

    implementado alguns métodos http

--- tpte2/Produtos.API/Controllers/ProdutoController.cs
+++ tpte2/Produtos.API/Controllers/ProdutoController.cs
@@ -17,12 +17,41 @@ namespace Produtos.API.Controllers
             return context.Produtos!.ToList();
         }
 
+        [HttpGet("/{id:int}")]
+        public ProdutoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            return context.Produtos!.FirstOrDefault(x => x.ProdutoId == id);
+        }
+
         [HttpPost("/")]
         public ProdutoModel Post([FromBody] ProdutoModel produtoModel, [FromServices] AppDbContext context)
         {
             context.Produtos!.Add(produtoModel);
             context.SaveChanges();
             return produtoModel;
+        }
+
+        [HttpPut("/")]
+        public ProdutoModel Put([FromRoute] int id, [FromBody] ProdutoModel produtoModel, [FromServices] AppDbContext context)
+        {
+            var model = context.Produtos!.FirstOrDefault(x => x.ProdutoId == id);
+            if (model == null) {
+                return produtoModel;
+            }
+
+            model.Nome = produtoModel.Nome;
+            model.Preco = produtoModel.Preco;
+            model.Quantidade = produtoModel.Quantidade;
+
+            context.Produtos!.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+
+        [HttpDelete("/")]
+        public ProdutoModel Delete([FromRoute] int id, [FromServices] AppDbContext context)
+        {
+            
         }
     }
 }
